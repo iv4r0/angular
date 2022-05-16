98889d0e6856aa96f58d8b60f52862877a137a97
Ivaro <ivaro@outlook.com.pe>
2022-05-16 17:14:06 -0500

    Implement Form for get data

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder } from '@angular/forms';
+
 import { CartService } from '../cart.service';
 import { Product } from '../product';
 
@@ -10,13 +12,29 @@ import { Product } from '../product';
 export class CartComponent implements OnInit {
   
   items:Product[] = [];
+  checkoutForm:any;
 
   constructor(
-    private cartService:CartService
-    ) { }
+    private cartService:CartService,
+    private formBuilder: FormBuilder,
+    ) {
+      this.checkoutForm = this.formBuilder.group({
+        name: '',
+        address: ''
+      });
+    }
 
   ngOnInit() {
     this.items = this.cartService.getItems();
+  }
+
+  onSubmit(customerData:any){
+
+    // Process checkout data here
+    this.items = this.cartService.clearCart();
+    this.checkoutForm.reset();
+
+    console.warn('Your order has been submitted', customerData);
   }
 
 }

ac1686bbdda1817938a6a98c4d6d28807094be9b
Ivaro <ivaro@outlook.com.pe>
2022-05-16 19:48:58 -0500

    Project Finalized

---
+++ 1_shopping_cart/src/app/mock-products.ts
@@ -0,0 +1,20 @@
+
+import { Product } from "./product";
+
+export const products:Product[] = [
+    {
+        name: 'Phone Zeus',
+        price: 650.90,
+        description: 'A large phone with one of the best screens'
+    },
+    {
+        name: 'Phone Cratos',
+        price: 999.90,
+        description: 'A great phone with one of the best cameras'
+    },
+    {
+        name: 'Phone Atenea',
+        price: 1599.90,
+        description: ''
+    }
+];

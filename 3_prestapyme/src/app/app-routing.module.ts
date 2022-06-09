2e573741a8072c3e24ce80898cd599ebdac8d8ae
iv4r0 <ivaro@outlook.com.pe>
2022-06-09 00:06:47 -0500

    Files and folders sorted

--- 3_prestapyme/src/app/app-routing.module.ts
+++ 3_prestapyme/src/app/app-routing.module.ts
@@ -1,14 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { LoginComponent } from './login/login.component';
-import { HomeComponent } from './home/home.component';
-import { ClientNewComponent } from './client-new/client-new.component';
-import { ClientListComponent } from './client-list/client-list.component';
-import { ClientSearchComponent } from './client-search/client-search.component';
-import { UserNewComponent } from './user-new/user-new.component';
-import { UserListComponent } from './user-list/user-list.component';
-import { UserSearchComponent } from './user-search/user-search.component';
-import { UserUpdateComponent } from './user-update/user-update.component';
+import { LoginComponent } from './components/acces-control/login/login.component';
+import { HomeComponent } from './components/dashboard/home/home.component';
+import { ClientNewComponent } from './components/clients/client-new/client-new.component';
+import { ClientListComponent } from './components/clients/client-list/client-list.component';
+import { ClientSearchComponent } from './components/clients/client-search/client-search.component';
+import { UserNewComponent } from './components/users/user-new/user-new.component';
+import { UserListComponent } from './components/users/user-list/user-list.component';
+import { UserSearchComponent } from './components/users/user-search/user-search.component';
+import { UserUpdateComponent } from './components/users/user-update/user-update.component';
 
 const routes: Routes = [
   {path:'', redirectTo:'/login', pathMatch: 'full'},
@@ -20,7 +20,7 @@ const routes: Routes = [
   {path: 'user-new', component: UserNewComponent},
   {path: 'user-list', component: UserListComponent},
   {path: 'user-search', component: UserSearchComponent},
-  {path: 'user-update', component: UserUpdateComponent}
+  {path: 'user-update', component: UserUpdateComponent }
 ];
 
 @NgModule({
